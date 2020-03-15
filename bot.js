7c193989fcb28eeb82c763572e71fad5ef52e904
Clone711 <62192334+Clone711@users.noreply.github.com>
2020-03-15 01:55:06 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+	var msg = args[1];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'porno':
+                bot.sendMessage({
+                    to: "686989112909103154",
+                    message: msg
+                });
+            	break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
